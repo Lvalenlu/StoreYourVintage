b83db8028be7a2edc1421b0d56a7a0d003b06763
Laura Henao <lvalenlu02@gmail.com>
2024-09-20 01:13:51 -0500

    Lau Cambio Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'StoreYourVintage') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}">
 
     <!-- enlaces-->
     <link rel="stylesheet" href="{{asset('css/styles_navbar.css')}}">
